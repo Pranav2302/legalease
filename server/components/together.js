c45fae87e1c594d41cf05fe890c673177dd54721
Pranav2302 <Pranavkamble663@gmail.com>
2024-09-12 01:26:27 +0530

    signup pages

---
+++ server/components/together.js
@@ -0,0 +1,8 @@
+import Logo from "./Logoimage/templogo.png";
+import Login from "./Login";
+import Signup from "./Signup";
+export {
+    Logo,
+    Login,
+    Signup
+}
---
+++ server/pages/Login.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import {Login as Logincomp} from "../components";
+function Login(){
+    return(
+    <div>
+        <Logincomp/>
+    </div>
+    )
+}
